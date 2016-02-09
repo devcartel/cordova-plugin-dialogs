c210f1a74ea83979763191cbaa2ac8c369c55126
ukrit-himakoon <ukrit.h@devcartel.com>
2016-02-09 15:56:35 +0700

    added dcprompt

--- src/ios/DCNotification.m
+++ src/ios/DCNotification.m
@@ -167,6 +167,17 @@ static NSMutableArray *alertList = nil;
 {
     NSString* callbackId = command.callbackId;
     NSString* message = [command argumentAtIndex:0];
+    NSString* title = [command argumentAtIndex:1];
+    NSArray* buttons = [command argumentAtIndex:2];
+    NSString* defaultText = [command argumentAtIndex:3];
+
+    [self showDialogWithMessage:message title:title buttons:buttons defaultText:defaultText callbackId:callbackId dialogType:DIALOG_TYPE_PROMPT];
+}
+
+- (void)dcprompt:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = command.callbackId;
+    NSString* message = [command argumentAtIndex:0];
     //NSString* title = [command argumentAtIndex:1];
     NSString* title = @"TEST TITLE";
     NSArray* buttons = [command argumentAtIndex:2];
